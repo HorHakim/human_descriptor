4a7e4ca40352d84232f19d6e1a828f9407b2b244
Hakim Horairy <horairyhakim@gmail.com>
2025-05-16 09:57:03 +0200

    upload une photo V1.1

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Prendre une photo avec l'appareil photo")
+st.title("Prends une photo en haute qualité")
 
-# Composant pour capturer une image
-photo = st.camera_input("Prenez une photo")
+uploaded_file = st.file_uploader(
+    "Prends ou choisis une photo", 
+    type=["jpg", "jpeg", "png"]
+)
 
-# Affichage ou traitement de la photo
-if photo:
-    image = Image.open(photo)
-    st.image(image, caption="Voici votre photo", use_container_width=True)
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Image importée", use_column_width=True)
